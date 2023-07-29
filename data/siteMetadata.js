04e3e0b29fee98f1b7e177adf7592f22c94fed20
sgktmk <sgktmk@gmail.com>
2023-07-29 15:51:01 +0900

    メタ情報の書き換え

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,22 +1,19 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
-  theme: 'system', // system, dark or light
+  title: 'Bikpela Poteto Bilong Mi',
+  author: 'sgktmk',
+  headerTitle: 'Bikpela Poteto Bilong Mi',
+  description: 'その大きな芋は私のものです。ありがとう。',
+  language: 'jp-ja',
+  theme: 'light', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  locale: 'en-US',
+  email: 'me@sgktmk.com',
+  github: 'https://github.com/sgktmk',
+  twitter: 'https://twitter.com/sgktmk',
+  locale: 'ja-JP',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
