bed08f172e9adef828025ac91c109fd31e3c6578
sgktmk <sgktmk@gmail.com>
2024-09-28 15:37:13 +0900

    結構ごちゃついたので削った（レスポンシブ対応）

--- components/MusicPlayer.js
+++ components/MusicPlayer.js
@@ -3,36 +3,17 @@ import ABCJS from 'abcjs'
 
 const MusicPlayer = ({ abcNotation }) => {
   const sheetRef = useRef(null)
-  const [parentSize, setParentSize] = useState({ width: 0, height: 0 })
 
   useEffect(() => {
-    const updateSize = () => {
-      if (sheetRef.current && sheetRef.current.parentElement) {
-        const { width, height } = sheetRef.current.parentElement.getBoundingClientRect()
-        setParentSize({ width, height })
-      }
-    }
+    const visualObj = ABCJS.renderAbc(sheetRef.current, abcNotation, {
+      responsive: 'resize',
+      expandToWidest: true,
+    })
+    playMusic(visualObj[0])
+  }, [abcNotation])
 
-    updateSize()
-    window.addEventListener('resize', updateSize)
-
-    return () => window.removeEventListener('resize', updateSize)
-  }, [])
-
-  useEffect(() => {
-    if (sheetRef.current && parentSize.width > 0) {
-      ABCJS.renderAbc(sheetRef.current, abcNotation, {
-        responsive: 'resize',
-        width: parentSize.width,
-        expandToWidest: true,
-      })
-      playMusic()
-    }
-  }, [abcNotation, parentSize])
-
-  const playMusic = () => {
+  const playMusic = (visualObj) => {
     if (ABCJS.synth.supportsAudio()) {
-      const visualObj = ABCJS.renderAbc(sheetRef.current, abcNotation)
       const synthControl = new ABCJS.synth.SynthController()
       synthControl.load('#audio', null, {
         displayLoop: true,
@@ -44,9 +25,9 @@ const MusicPlayer = ({ abcNotation }) => {
 
       const createSynth = async () => {
         const synth = new ABCJS.synth.CreateSynth()
-        await synth.init({ visualObj: visualObj[0] })
+        await synth.init({ visualObj: visualObj })
         await synth.prime()
-        synthControl.setTune(visualObj[0], false)
+        synthControl.setTune(visualObj, false)
       }
 
       createSynth()
@@ -56,8 +37,8 @@ const MusicPlayer = ({ abcNotation }) => {
   }
 
   return (
-    <div style={{ width: '100%', height: '100%' }}>
-      <div ref={sheetRef} style={{ width: '100%', height: 'auto' }}></div>
+    <div>
+      <div ref={sheetRef}></div>
       <div id="audio"></div>
     </div>
   )
