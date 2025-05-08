acc55220b160964d074f8f160de7380b59a7a7db
sgktmk <sgktmk@gmail.com>
2025-05-08 13:00:21 +0000

    謎空白を削除

--- next.config.js
+++ next.config.js
@@ -3,15 +3,15 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 // You might need to insert additional domains in script-src if you are using external services
-const ContentSecurityPolicy = `  
-  default-src 'self';  
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app www.googletagmanager.com;  
-  style-src 'self' 'unsafe-inline';  
-  img-src * blob: data:;  
-  media-src 'none';  
-  connect-src * www.google-analytics.com;  
-  font-src 'self';  
-  frame-src giscus.app  
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app www.googletagmanager.com;
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data:;
+  media-src 'none';
+  connect-src * www.google-analytics.com;
+  font-src 'self';
+  frame-src giscus.app
 `
 
 const securityHeaders = [
