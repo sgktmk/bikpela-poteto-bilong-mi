9d088d900c79fe63568fe9fbeac165de71b2d70d
sgktmk <me@sgktmk.com>
2025-06-15 16:42:20 +0900

    Add Discord and connpass social icons

--- components/social-icons/index.js
+++ components/social-icons/index.js
@@ -4,6 +4,8 @@ import Facebook from './facebook.svg'
 import Youtube from './youtube.svg'
 import Linkedin from './linkedin.svg'
 import Twitter from './twitter.svg'
+import Discord from './discord.svg'
+import Connpass from './connpass.svg'
 
 // Icons taken from: https://simpleicons.org/
 
@@ -14,6 +16,8 @@ const components = {
   youtube: Youtube,
   linkedin: Linkedin,
   twitter: Twitter,
+  discord: Discord,
+  connpass: Connpass,
 }
 
 const SocialIcon = ({ kind, href, size = 8 }) => {
--- data/authors/default.md
+++ data/authors/default.md
@@ -5,6 +5,8 @@ occupation: Web Engineer, Engineering Manager (Rookie)
 email: me@sgktmk.com
 twitter: https://x.com/sgktmk_com
 github: https://github.com/sgktmk
+discord: ''
+connpass: ''
 ---
 
-福井県で楽器を吹いたり弾いたり聴いたり、プログラムを書いたり書かなかったりしています。  
+福井県で楽器を吹いたり弾いたり聴いたり、プログラムを書いたり書かなかったりしています。
